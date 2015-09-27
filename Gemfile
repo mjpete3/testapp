1a2fc4a72c17898653ee2a2213415d9e8c664999
marty <themooseman@comcast.net>
2015-09-27 16:19:47 -0400

    Updated test app

--- Gemfile
+++ Gemfile
@@ -1,15 +1,12 @@
 source 'https://rubygems.org'
 
-gem 'rails', '3.2.7'
-gem 'thin'
+gem 'rails', '3.2.21'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
 gem 'jquery-rails'
-gem 'net-sftp'
-
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -19,20 +16,8 @@ group :assets do
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   gem 'therubyracer', :platforms => :ruby
-
   gem 'uglifier', '>= 1.0.3'
 end
-
-
-
-# To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
-# To use Jbuilder templates for JSON
-# gem 'jbuilder'
-
-# Use unicorn as the app server
-# gem 'unicorn'
 
 # Deploy with Capistrano
 # gem 'capistrano'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,81 +1,15 @@
-require 'net/sftp'
+require 'socket'
 
 class HomeController < ApplicationController
   
-  def index
-    @message = "Welcome to the Test App"
-  end
-  
-  def show
-    @sftp_address = ''
-    @sftp_user_id = ''
-    @sftp_password = ''
-    @sftp_port = 22
-    @sftp_send = '/outbound'
-    @sftp_receive = '/inbound'
-    
-    Rails.logger.error "Starting"
-    @message = []
-    @message.push "Starting"
-    begin
-      Net::SFTP.start(@sftp_address, @sftp_user_id, :password => @sftp_password, :port => @sftp_port ) do |sftp|
-        Rails.logger.error "Connection to #{@sftp_address} created with user: #{@sftp_user_id}"
-        @message.push "Connection to #{@sftp_address} created with user: #{@sftp_user_id}"
-        #check for the folder sending data to the vendor
-        check_outbound(sftp)
-        check_inbound(sftp)
-      end    
-    rescue SocketError => e
-          Rails.logger.error "Issue with the SFTP connection => #{e.message}"
-          @message.push "Issue with the SFTP connection => #{e.message}"
-    rescue Net::SSH::AuthenticationFailed => e
-          Rails.logger.error "User name or password is incorrect => #{e.message}"
-          @message.push "User name or password is incorrect => #{e.message}"
-    rescue Net::SSH::Authentication::AgentError => e
-          Rails.logger.error "User name or password is incorrect => #{e.message}"
-          @message.push "User name or password is incorrect => #{e.message}"          
-    rescue Errno::ETIMEDOUT => e
-          Rails.logger.error "Possible error with the port or other SFTP settings => #{e.message}"
-          @message.push "Possible error with the port or other SFTP settings => #{e.message}"          
-    rescue Net::SFTP::StatusException => e
-          Rails.logger.error "A Status exception error occured => #{e.message}"
-          @message.push "A Status exception error occured => #{e.message}"          
-    rescue => e
-          Rails.logger.error "A general error occured.  Please check your settings. => #{e.message}"          
-          @message.push "A general error occured.  Please check your settings. => #{e.message}"
-          @message.push "#{e.inspect}"     
+  def index       
+    @hostname = Socket.gethostname
+    @ip_address = Socket.ip_address_list.find { |ai| ai.ipv4? && !ai.ipv4_loopback? }.ip_address
+    @public_address = Socket.ip_address_list.detect{|intf| intf.ipv4? and !intf.ipv4_loopback? and !intf.ipv4_multicast? and !intf.ipv4_private?}
+    respond_to do |format|
+      format.html
     end
   end
   
-  
-  private
-  
-  def check_outbound(sftp)
-      sftp.lstat!(@sftp_send) do |response|
-        if response.ok?
-          Rails.logger.error "The send to folder: #{@sftp_send} exists."           
-          @message.push "The send to folder: #{@sftp_send} exists."
-        else
-          status = false
-          Rails.logger.error "The send to folder: #{@sftp_send} is not correct"
-          @message.push "The send to folder: #{@sftp_send} is not correct"
-        end
-      end
-    
-  end
-  
-  def check_inbound(sftp)
-      #check for the folder receiving data from the vendor
-      sftp.lstat!(@sftp_receive) do |response|
-        if response.ok?
-          Rails.logger.error "The receive from folder: #{@sftp_receive} exists."
-          @message.push "The receive from folder: #{@sftp_receive} exists."
-        else
-          status = false
-          Rails.logger.error "The receive from folder: #{@sftp_receive} is not correct"
-          @message.push "The receive from folder: #{@sftp_receive} is not correct"
-        end                  
-      end    
-  end
   
 end
